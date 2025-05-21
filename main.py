af4680357dad70bfffa171f721b526e17b42a219
CoCo9122 <banaji.yu0517@outlook.jp>
2025-05-22 02:48:12 +0900

    fastapiの初期設定

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.v1.api import api_router
+import os
+
+app = FastAPI(
+    title="FURNIAIZER API",
+    version="1.0.0"
+)
+
+origins = ["http://localhost:3000"]
+
+if os.getenv("ALLOWED_ORIGINS"):
+    origins.extend(os.getenv("ALLOWED_ORIGINS").split(","))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,     # List of allowed origins
+    allow_credentials=True,    # Allow cookies
+    allow_methods=["*"],       # Allow all methods
+    allow_headers=["*"],       # Allow all headers
+)
+
+app.include_router(api_router, prefix="/api/v1")
